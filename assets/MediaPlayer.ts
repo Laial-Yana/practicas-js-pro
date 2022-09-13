589b7572d3f4e412a5bd5e394cec6c84db5b8991
Laial-Yana <laial@yana.com.mx>
2022-09-13 18:08:45 -0300

    40th class: Add Ads
    Through the use of plugins where each expose the run interface we can extend the base functionality of MediaPlayer staying out of its internal code

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,23 +1,24 @@
 export default class MediaPlayer {
   media: HTMLMediaElement;
   plugins: Array<any>;
+  container: HTMLElement;
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
+    this.initPlayer();
     this.initPlugins();
   }
+  initPlayer() {
+    this.container = document.createElement("div");
+    this.container.style.position = "relative";
+    //tenemos que mover media dentro del contenedor
+    // primero tenemos que ubicar el contenedor como pariente de media y con insertBefore
+    // ubicamos a media justo al lado del container
+    this.media.parentNode?.insertBefore(this.container, this.media);
+    //ahora podemos decir que el container va a contener media
+    this.container.appendChild(this.media);
+  }
   private initPlugins() {
-    // const player = {
-    //   play: () => this.play(),
-    //   pause: () => this.pause(),
-    //   media: this.media,
-    //   get muted() {
-    //     return this.media.muted;
-    //   },
-    //   set muted(value) {
-    //     this.media.muted = value;
-    //   },
-    // };
     this.plugins.forEach((p) => {
       p.run(this);
     });
--- assets/plugins/Ads/index.ts
+++ assets/plugins/Ads/index.ts
@@ -5,13 +5,17 @@ export default class AdsPlugin {
   private ads: Ads;
   private player: MediaPlayer;
   private media: HTMLElement;
-  private currentAdd: Ad;
+  private currentAdd: Ad | null;
+  private adsContainer: HTMLElement;
   constructor() {
     this.ads = Ads.getInstance();
+    //creamos contenedor que va a contener los ads
+    this.adsContainer = document.createElement("div");
     this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
   }
   run(player: MediaPlayer) {
     this.player = player;
+    this.player.container.appendChild(this.adsContainer);
     this.media = this.player.media;
     this.media.addEventListener("timeupdate", this.handleTimeUpdate);
   }
@@ -30,6 +34,20 @@ export default class AdsPlugin {
     const ad = this.ads.getAd();
     //llevar registro de cual es el add actual
     this.currentAdd = ad!;
-    console.log(this.currentAdd);
+    this.adsContainer.innerHTML = `
+    <div class="ads">
+      <a class="ads__link" href="${this.currentAdd.url}" target="_blank">
+        <img class="ads__img" src="${this.currentAdd.imageUrl}" />
+        <div class="ads__info">
+          <h5 class="ads__title">${this.currentAdd.title}</h5>
+          <p class="ads__body">${this.currentAdd.body}</p>
+        </div>
+      </a>
+    </div>
+  `;
+    setTimeout(() => {
+      this.currentAdd = null;
+      this.adsContainer.innerHTML = "";
+    }, 10000);
   }
 }
