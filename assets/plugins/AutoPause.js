fb51d62f792a2c733184b1d0d208f93953f666af
Laial-Yana <laial@yana.com.mx>
2022-09-13 03:27:20 -0300

    22nd class: VisibilityChange
    In this class we add a new method of AutoPause plugin where it automatically pauses when tab change occurs using DOM's VisibilityChange api

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,26 +1,35 @@
 export default class AutoPause {
   constructor(player) {
     this.threshold = 0.25;
-    this.handleIntersection = this.handleIntersection.bind(this); // decimos que siempre que se llame a handleIntersection
-    // this se va a referir a la instancia de la clase AutoPause
+    this.handleIntersection = this.handleIntersection.bind(this);
+    //aca agrega esta linea para volver a direccionar el this como en la linea anterior,
+    //pero esto no fue necesario para que la funcion se ejecute correctamente, no me saltó el error de
+    //"trying to read properties of undefined: reading pause", igual lo hice.
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
   run(player) {
     this.player = player;
-    // console.log(player, "Dentro de la funcion run");
     const observer = new IntersectionObserver(this.handleIntersection, {
-      threshold: this.threshold, //el porcentaje de pantalla que el video tiene que
-      // pasar para que el observer nos avise
+      threshold: this.threshold,
     });
-    observer.observe(this.player.media); // el elemento al que va a observar el observer
-    // y el contenedor es toda la pantalla.
+    observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange", () => {
+      //aqui agregamos un event listener para observar cuando se cambia de pestaña
+      this.handleVisibilityChange();
+    });
   }
-  //entries es una lista/array que obverser le pasa al handleIntersection cuando lo ejecuta
-  //contiene todos los elementos que se estan observando
   handleIntersection(entries) {
-    // console.log(player, "Dentro de la funcion handleintersection");
     let entry = entries[0];
-    // console.log(entry, "ENTRY");
     const isVisible = entry.intersectionRatio >= this.threshold;
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === "visible";
     if (isVisible) {
       this.player.play();
     } else {
